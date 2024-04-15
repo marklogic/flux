5ed9e30d8836e21e7ac03a58caa7ade98fc86687
Rob Rudin <rudin@progress.com>
2024-04-15 09:52:54 -0400

    MLE-12632 Added abortOnReadFailure for commands using Spark readers
    The tests primarily capture the behavior of various Spark configuration options.

--- new-tool-cli/src/main/java/com/marklogic/newtool/command/ReadFilesParams.java
+++ new-tool-cli/src/main/java/com/marklogic/newtool/command/ReadFilesParams.java
@@ -34,10 +34,11 @@ public class ReadFilesParams {
     public Map<String, String> makeOptions() {
         Map<String, String> options = new HashMap<>();
         if (abortOnReadFailure != null && abortOnReadFailure) {
-            options.put(Options.READ_FILES_ABORT_ON_FAILURE, "true");
+            addOptionsToFailOnReadError(options);
         } else {
-            options.put(Options.READ_FILES_ABORT_ON_FAILURE, "false");
+            addOptionsToNotFailOnReadError(options);
         }
+
         if (filter != null) {
             options.put("pathGlobFilter", filter);
         }
@@ -45,6 +46,34 @@ public class ReadFilesParams {
             options.put("recursiveFileLookup", recursiveFileLookup.toString());
         }
         return options;
+    }
+
+    /**
+     * For the Spark data sources, we don't get the ideal user experience here, as we haven't figured out if it's
+     * possible to configure Spark to log something about failed files/rows, which we're able to do with our own
+     * connector. So the Spark reader will skip over invalid data without any logging for the user to see. This will
+     * likely be fine for a scenario such as trying to read Avro files with a Parquet reader, where the user shouldn't
+     * have an expectation that the Avro files are processed correctly.
+     *
+     * @param options
+     */
+    private void addOptionsToNotFailOnReadError(Map<String, String> options) {
+        // For commands that use the MarkLogic connector to read files.
+        options.put(Options.READ_FILES_ABORT_ON_FAILURE, "false");
+        // For commands that use Spark JSON / CSV.
+        options.put("mode", "DROPMALFORMED");
+        // For commands that use Spark Parquet / Avro / ORC. This does not appear to affect Spark JSON / CSV, as
+        // those use the 'mode' option to determine what to do with corrupt data.
+        options.put("ignoreCorruptFiles", "true");
+    }
+
+    private void addOptionsToFailOnReadError(Map<String, String> options) {
+        // For commands that use the MarkLogic connector to read files.
+        options.put(Options.READ_FILES_ABORT_ON_FAILURE, "true");
+        // For commands that use Spark JSON / CSV to read files.
+        options.put("mode", "FAILFAST");
+        // For commands that use Spark Parquet / Avro / ORC.
+        options.put("ignoreCorruptFiles", "false");
     }
 
     public List<String> getPaths() {
--- new-tool-cli/src/test/java/com/marklogic/newtool/command/ImportOrcFilesTest.java
+++ new-tool-cli/src/test/java/com/marklogic/newtool/command/ImportOrcFilesTest.java
@@ -6,8 +6,7 @@ import com.marklogic.client.io.StringHandle;
 import com.marklogic.newtool.AbstractTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class ImportOrcFilesTest extends AbstractTest {
@@ -82,6 +81,37 @@ class ImportOrcFilesTest extends AbstractTest {
         assertTrue(stderr.contains("spark.sql.parquet.filterPushdown should be boolean, but was invalid-value"),
             "This test verifies that spark.sql dynamic params are added to the Spark conf. An invalid value is used " +
                 "to verify this, as its inclusion in the Spark conf should cause an error. Actual stderr: " + stderr);
+    }
+
+    @Test
+    void dontAbortOnReadFailure() {
+        String stderr = runAndReturnStderr(() -> run(
+            "import_orc_files",
+            "--path", "src/test/resources/orc-files/authors.orc",
+            "--path", "src/test/resources/avro/colors.avro",
+            "--clientUri", makeClientUri(),
+            "--permissions", DEFAULT_PERMISSIONS,
+            "--collections", "orc-data"
+        ));
+
+        assertCollectionSize("The authors.orc file should have been processed correctly", "orc-data", 15);
+        assertFalse(stderr.contains("Command failed"), "The command should default to ignoreCorruptFiles=true for " +
+            "reading ORC files so that invalid files do not cause the command to fail; stderr: " + stderr);
+    }
+
+    @Test
+    void abortOnReadFailure() {
+        String stderr = runAndReturnStderr(() -> run(
+            "import_parquet_files",
+            "--path", "src/test/resources/avro/colors.avro",
+            "--abortOnReadFailure",
+            "--clientUri", makeClientUri(),
+            "--permissions", DEFAULT_PERMISSIONS
+        ));
+
+        assertTrue(stderr.contains("Command failed") && stderr.contains("Could not read footer for file"),
+            "The command should have failed because Spark could not read the footer of the invalid Avro file; " +
+                "stderr: " + stderr);
     }
 
     private void verifyDocContent(String uri) {
