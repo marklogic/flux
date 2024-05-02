2906dc2a00e2d3f68cfc3fc114fc97dd24aab14e
Rob Rudin <rudin@progress.com>
2024-05-02 11:41:03 -0400

    Refactor: Reduced duplication across commands for structured files

--- new-tool-cli/src/main/java/com/marklogic/newtool/command/importdata/ImportDelimitedFilesCommand.java
+++ new-tool-cli/src/main/java/com/marklogic/newtool/command/importdata/ImportDelimitedFilesCommand.java
@@ -1,11 +1,7 @@
 package com.marklogic.newtool.command.importdata;
 
 import com.beust.jcommander.DynamicParameter;
-import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
-import com.beust.jcommander.ParametersDelegate;
-import com.marklogic.newtool.command.OptionsUtil;
-import com.marklogic.spark.Options;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,48 +9,26 @@ import java.util.Map;
 @Parameters(commandDescription = "Read delimited text files from local, HDFS, and S3 locations using Spark's support " +
     "defined at https://spark.apache.org/docs/latest/sql-data-sources-csv.html, with each row being written " +
     "as a JSON document to MarkLogic.")
-public class ImportDelimitedFilesCommand extends AbstractImportFilesCommand {
-
-    @Parameter(
-        names = "--jsonRootName",
-        description = "Name of a root field to add to each JSON document."
-    )
-    private String jsonRootName;
-
-    @ParametersDelegate
-    private XmlDocumentParams xmlDocumentParams = new XmlDocumentParams();
+public class ImportDelimitedFilesCommand extends AbstractImportStructuredFilesCommand {
 
     @DynamicParameter(
         names = "-P",
         description = "Specify any Spark CSV option defined at " +
             "https://spark.apache.org/docs/latest/sql-data-sources-csv.html; e.g. -PquoteAll=true."
     )
-    private Map<String, String> csvParams = new HashMap<>();
+    private Map<String, String> csvParams;
 
-    @Override
-    protected String getReadFormat() {
-        return "csv";
+    public ImportDelimitedFilesCommand() {
+        super("csv");
+        csvParams = new HashMap<>();
+        setDynamicParams(csvParams);
     }
 
     @Override
     protected Map<String, String> makeReadOptions() {
         Map<String, String> options = super.makeReadOptions();
-        options.putAll(csvParams);
-        if (!options.containsKey("header")) {
-            options.put("header", "true");
-        }
-        if (!options.containsKey("inferSchema")) {
-            options.put("inferSchema", "true");
-        }
-        return options;
-    }
-
-    @Override
-    protected Map<String, String> makeWriteOptions() {
-        Map<String, String> options = OptionsUtil.addOptions(super.makeWriteOptions(),
-            Options.WRITE_JSON_ROOT_NAME, jsonRootName
-        );
-        options.putAll(xmlDocumentParams.makeOptions());
+        options.putIfAbsent("header", "true");
+        options.putIfAbsent("inferSchema", "true");
         return options;
     }
 }
--- new-tool-cli/src/main/java/com/marklogic/newtool/command/importdata/ImportOrcFilesCommand.java
+++ new-tool-cli/src/main/java/com/marklogic/newtool/command/importdata/ImportOrcFilesCommand.java
@@ -1,12 +1,7 @@
 package com.marklogic.newtool.command.importdata;
 
 import com.beust.jcommander.DynamicParameter;
-import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
-import com.beust.jcommander.ParametersDelegate;
-import com.marklogic.newtool.command.OptionsUtil;
-import com.marklogic.spark.Options;
-import org.apache.spark.sql.SparkSession;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,16 +9,7 @@ import java.util.Map;
 @Parameters(commandDescription = "Read ORC files from local, HDFS, and S3 locations using Spark's support " +
     "defined at https://spark.apache.org/docs/latest/sql-data-sources-orc.html, with each row being " +
     "written as a JSON document in MarkLogic.")
-public class ImportOrcFilesCommand extends AbstractImportFilesCommand {
-
-    @Parameter(
-        names = "--jsonRootName",
-        description = "Name of a root field to add to each JSON document."
-    )
-    private String jsonRootName;
-
-    @ParametersDelegate
-    private XmlDocumentParams xmlDocumentParams = new XmlDocumentParams();
+public class ImportOrcFilesCommand extends AbstractImportStructuredFilesCommand {
 
     @DynamicParameter(
         names = "-P",
@@ -31,35 +17,11 @@ public class ImportOrcFilesCommand extends AbstractImportFilesCommand {
             "https://spark.apache.org/docs/latest/sql-data-sources-orc.html; e.g. -PmergeSchema=true or " +
             "-Pspark.sql.orc.filterPushdown=false."
     )
-    private Map<String, String> orcParams = new HashMap<>();
+    private Map<String, String> orcParams;
 
-    @Override
-    protected String getReadFormat() {
-        return "orc";
-    }
-
-    @Override
-    protected void modifySparkSession(SparkSession session) {
-        orcParams.entrySet().stream()
-            .filter(OptionsUtil::isSparkConfigurationOption)
-            .forEach(entry -> session.conf().set(entry.getKey(), entry.getValue()));
-    }
-
-    @Override
-    protected Map<String, String> makeReadOptions() {
-        Map<String, String> options = super.makeReadOptions();
-        orcParams.entrySet().stream()
-            .filter(OptionsUtil::isSparkDataSourceOption)
-            .forEach(entry -> options.put(entry.getKey(), entry.getValue()));
-        return options;
-    }
-
-    @Override
-    protected Map<String, String> makeWriteOptions() {
-        Map<String, String> options = OptionsUtil.addOptions(super.makeWriteOptions(),
-            Options.WRITE_JSON_ROOT_NAME, jsonRootName
-        );
-        options.putAll(xmlDocumentParams.makeOptions());
-        return options;
+    public ImportOrcFilesCommand() {
+        super("orc");
+        orcParams = new HashMap<>();
+        setDynamicParams(orcParams);
     }
 }
