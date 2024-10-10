cc19df8fe0755589b5b2f332904844badb8e0696
Rob Rudin <rudin@progress.com>
2024-10-10 10:08:04 -0400

    Initial splitter support
    This is just adding the bare minimum to a single command for easy demo'ing.

---
+++ flux-cli/src/main/java/com/marklogic/flux/impl/importdata/SplitterParams.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2024 MarkLogic Corporation. All Rights Reserved.
+ */
+package com.marklogic.flux.impl.importdata;
+
+import com.marklogic.flux.impl.OptionsUtil;
+import com.marklogic.spark.Options;
+import org.jdom2.Namespace;
+import picocli.CommandLine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class SplitterParams implements CommandLine.ITypeConverter<Namespace> {
+
+    @CommandLine.Option(names = "--splitter-xml-path")
+    private String xmlPath;
+
+    @CommandLine.Option(
+        names = "--splitter-xml-namespace",
+        converter = SplitterParams.class
+    )
+    private List<Namespace> xmlNamespaces = new ArrayList<>();
+
+    @CommandLine.Option(names = "--splitter-max-chunk-size")
+    private Integer maxChunkSize;
+
+    @CommandLine.Option(names = "--splitter-max-overlap-size")
+    private Integer maxOverlapSize;
+
+    @CommandLine.Option(names = "--splitter-text")
+    private boolean text;
+
+    public Map<String, String> makeOptions() {
+        Map<String, String> options = OptionsUtil.makeOptions(
+            Options.WRITE_SPLITTER_XML_PATH, xmlPath,
+            Options.WRITE_SPLITTER_MAX_CHUNK_SIZE, OptionsUtil.integerOption(maxChunkSize),
+            Options.WRITE_SPLITTER_MAX_OVERLAP_SIZE, OptionsUtil.integerOption(maxOverlapSize),
+            Options.WRITE_SPLITTER_TEXT, text ? "true" : null
+        );
+
+        xmlNamespaces.forEach(namespace ->
+            options.put(Options.WRITE_SPLITTER_XML_NAMESPACE_PREFIX + namespace.getPrefix(), namespace.getURI()));
+
+        return options;
+    }
+
+    @Override
+    public Namespace convert(String value) {
+        // Needs error handling.
+        String[] tokens = value.split("=");
+        return Namespace.getNamespace(tokens[0], tokens[1]);
+    }
+}
