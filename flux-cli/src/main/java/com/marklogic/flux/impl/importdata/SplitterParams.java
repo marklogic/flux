a27ab009ee03177c1e2377b16146c8bd31eea93d
Rob Rudin <rudin@progress.com>
2024-10-11 15:16:31 -0400

    MLE-17215 Added options for JSON and regex
    Moved some existing JSON test files so it's easy to add more test files.

--- flux-cli/src/main/java/com/marklogic/flux/impl/importdata/SplitterParams.java
+++ flux-cli/src/main/java/com/marklogic/flux/impl/importdata/SplitterParams.java
@@ -3,6 +3,7 @@
  */
 package com.marklogic.flux.impl.importdata;
 
+import com.marklogic.flux.api.FluxException;
 import com.marklogic.flux.impl.OptionsUtil;
 import com.marklogic.spark.Options;
 import org.jdom2.Namespace;
@@ -11,8 +12,12 @@ import picocli.CommandLine;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SplitterParams implements CommandLine.ITypeConverter<Namespace> {
+
+    @CommandLine.Option(names = "--splitter-json-pointer")
+    private List<String> jsonPointer = new ArrayList<>();
 
     @CommandLine.Option(names = "--splitter-xml-path")
     private String xmlPath;
@@ -29,6 +34,19 @@ public class SplitterParams implements CommandLine.ITypeConverter<Namespace> {
     @CommandLine.Option(names = "--splitter-max-overlap-size")
     private Integer maxOverlapSize;
 
+    @CommandLine.Option(
+        names = "--splitter-regex",
+        description = "Regular expression for splitting the selected text into chunks."
+    )
+    private String regex;
+
+    @CommandLine.Option(
+        names = "--splitter-join-delimiter",
+        description = "Delimiter for joining chunks split via --splitter-regex when two or more consecutive " +
+            "chunks can be combined and still be smaller than the max chunk size."
+    )
+    private String joinDelimiter;
+
     @CommandLine.Option(names = "--splitter-text")
     private boolean text;
 
@@ -37,8 +55,14 @@ public class SplitterParams implements CommandLine.ITypeConverter<Namespace> {
             Options.WRITE_SPLITTER_XML_PATH, xmlPath,
             Options.WRITE_SPLITTER_MAX_CHUNK_SIZE, OptionsUtil.integerOption(maxChunkSize),
             Options.WRITE_SPLITTER_MAX_OVERLAP_SIZE, OptionsUtil.integerOption(maxOverlapSize),
-            Options.WRITE_SPLITTER_TEXT, text ? "true" : null
+            Options.WRITE_SPLITTER_TEXT, text ? "true" : null,
+            Options.WRITE_SPLITTER_REGEX, regex,
+            Options.WRITE_SPLITTER_JOIN_DELIMITER, joinDelimiter
         );
+
+        if (!jsonPointer.isEmpty()) {
+            options.put(Options.WRITE_SPLITTER_JSON_POINTERS, jsonPointer.stream().collect(Collectors.joining("\n")));
+        }
 
         xmlNamespaces.forEach(namespace ->
             options.put(Options.WRITE_SPLITTER_XML_NAMESPACE_PREFIX + namespace.getPrefix(), namespace.getURI()));
@@ -48,8 +72,10 @@ public class SplitterParams implements CommandLine.ITypeConverter<Namespace> {
 
     @Override
     public Namespace convert(String value) {
-        // Needs error handling.
         String[] tokens = value.split("=");
+        if (tokens.length % 2 != 0) {
+            throw new FluxException("The value must match the pattern prefix=namespaceURI");
+        }
         return Namespace.getNamespace(tokens[0], tokens[1]);
     }
 }
