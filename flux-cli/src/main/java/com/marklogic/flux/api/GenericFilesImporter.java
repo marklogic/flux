f44641c3ac2afb72f4ac7ac07dc39ae07d2d5b0c
Rob Rudin <rudin@progress.com>
2025-02-19 16:44:44 -0500

    Added extraction options, and no longer compiling against Java Client
    Started this branch with adding the necessary extraction options to the API.
    
    Then modified it to not have to compile against the Java Client, which is now fully shadowed in the marklogic-spark-connector.

--- flux-cli/src/main/java/com/marklogic/flux/api/GenericFilesImporter.java
+++ flux-cli/src/main/java/com/marklogic/flux/api/GenericFilesImporter.java
@@ -24,6 +24,31 @@ public interface GenericFilesImporter extends Executor<GenericFilesImporter> {
 
     interface WriteGenericDocumentsOptions extends WriteDocumentsOptions<WriteGenericDocumentsOptions> {
         WriteGenericDocumentsOptions documentType(DocumentType documentType);
+
+        /**
+         * @since 1.3.0
+         */
+        WriteGenericDocumentsOptions extractText();
+
+        /**
+         * @since 1.3.0
+         */
+        WriteGenericDocumentsOptions extractedTextDocumentType(DocumentType documentType);
+
+        /**
+         * @since 1.3.0
+         */
+        WriteGenericDocumentsOptions extractedTextCollections(String commaDelimitedCollections);
+
+        /**
+         * @since 1.3.0
+         */
+        WriteGenericDocumentsOptions extractedTextPermissionsString(String rolesAndCapabilities);
+
+        /**
+         * @since 1.3.0
+         */
+        WriteGenericDocumentsOptions extractedTextDropSource();
     }
 
     GenericFilesImporter from(Consumer<ReadGenericFilesOptions> consumer);
