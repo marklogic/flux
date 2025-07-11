5bb501fe2c0037085d3803ad45308b8e9fe64fbe
Rob Rudin <rudin@progress.com>
2025-07-11 10:38:23 -0400

    MLE-22889 Can now encode vectors

--- flux-cli/src/test/java/com/marklogic/flux/api/EmbedderOptionsTest.java
+++ flux-cli/src/test/java/com/marklogic/flux/api/EmbedderOptionsTest.java
@@ -27,6 +27,7 @@ class EmbedderOptionsTest extends AbstractOptionsTest {
                     .embeddingName("my-embedding")
                     .embeddingNamespace("org:example")
                     .batchSize(5)
+                    .base64Encode()
                     .prompt("Just testing")
                     .textJsonPointer("/json/text")
                     .textXPath("/xml/text");
@@ -44,7 +45,8 @@ class EmbedderOptionsTest extends AbstractOptionsTest {
             Options.WRITE_EMBEDDER_TEXT_JSON_POINTER, "/json/text",
             Options.WRITE_EMBEDDER_TEXT_XPATH, "/xml/text",
             Options.WRITE_EMBEDDER_BATCH_SIZE, "5",
-            Options.WRITE_EMBEDDER_PROMPT, "Just testing"
+            Options.WRITE_EMBEDDER_PROMPT, "Just testing",
+            Options.WRITE_EMBEDDER_BASE64_ENCODE, "true"
         );
     }
 }
