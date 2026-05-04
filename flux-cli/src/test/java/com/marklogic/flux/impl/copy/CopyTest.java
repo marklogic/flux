b7b5b8fb6375d7249562062a88e98a476357726b
Jason Teh <jteh@cas.org>
2026-05-04 12:36:35 +0100

    bump copyright header

--- flux-cli/src/test/java/com/marklogic/flux/impl/copy/CopyTest.java
+++ flux-cli/src/test/java/com/marklogic/flux/impl/copy/CopyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2025 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
+ * Copyright (c) 2024-2026 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
  */
 package com.marklogic.flux.impl.copy;
 
