9edb839effbafdc373fc57fe0db1158ff0675fdd
Rob Rudin <rudin@progress.com>
2026-05-07 15:20:16 -0400

    Added read method for Command
    For internal use.

--- flux-cli/src/main/java/com/marklogic/flux/impl/Command.java
+++ flux-cli/src/main/java/com/marklogic/flux/impl/Command.java
@@ -3,6 +3,8 @@
  */
 package com.marklogic.flux.impl;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import picocli.CommandLine;
 
@@ -12,6 +14,10 @@ public interface Command {
      * @param session the Spark session to use for executing the command
      */
     void execute(SparkSession session);
+
+    default Dataset<Row> read(SparkSession session) throws Exception {
+        throw new UnsupportedOperationException("read is not supported for this command");
+    }
 
     /**
      * With picocli, we don't have a JCommander-like facility for validating all the params before the command is
